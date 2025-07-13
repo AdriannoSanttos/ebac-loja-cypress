2830dd2b054ddc488d775eba45cad47533f7e3f0
Adriano Santtos <j.adriannosanttos@gmail.com>
2025-07-13 20:58:11 -0300

    produtos adicionados ao carrinho

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -10,13 +10,59 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
-  });
+      cy.visit('http://lojaebac.ebaconline.art.br/')
+  })
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+      //Login como cliente
+
+     cy.get('.icon-user-unfollow').click()
+     cy.get('#username').type('irmaodojorel@teste.com.br')
+     cy.get('#password').type('teste@001')
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, irmaodojorel')
+
+     // Escolher 4 produtos e adicionar ao carrinho
+
+     //primeiro produto 
+     cy.get('#primary-menu > .menu-item-629 > a').click()
+     cy.get('.post-2559').click()
+     cy.get('.button-variable-item-S').click()
+     cy.get('.button-variable-item-Red').click()
+     cy.get('.single_add_to_cart_button').click()
+     cy.get('.woocommerce-message').should('contain', '“Abominable Hoodie” foi adicionado no seu carrinho.')
+
+     //segundo produto 
+     cy.visit('/produtos')
+     cy.get('.post-3111').click()
+     cy.get('.button-variable-item-S').click()
+     cy.get('.button-variable-item-Brown').click()
+     cy.get('.single_add_to_cart_button').click()
+     cy.get('.woocommerce-message').should('contain', '“Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
+     
+     //terceiro produto 
+     cy.visit('/produtos')
+     cy.get('.post-3073 ').click()
+     cy.get('.button-variable-item-32').click()
+     cy.get('.button-variable-item-Brown').click()
+     cy.get('.single_add_to_cart_button').click()
+     cy.get('.woocommerce-message').should('contain', '“Aether Gym Pant” foi adicionado no seu carrinho.')
+
+      //quarto produto 
+     cy.visit('/produtos')
+     cy.get('.post-2622').click()
+     cy.get('.button-variable-item-L').click()
+     cy.get('.button-variable-item-Green').click()
+     cy.get('.single_add_to_cart_button').click()
+     cy.get('.woocommerce-message').should('contain', '“Ajax Full-Zip Sweatshirt” foi adicionado no seu carrinho.')
+     
+
+
+
+
+
       
-  });
+  })
 
 
 })
